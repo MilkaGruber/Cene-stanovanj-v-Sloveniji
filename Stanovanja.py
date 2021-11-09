dc4e30d00ba580561f725ecc5ad7aa85397a88a2
MilkaGruber <64840327+MilkaGruber@users.noreply.github.com>
2021-11-09 22:13:24 +0100

    Napisane funkcije za zajem podatkov

--- Stanovanja.py
+++ Stanovanja.py
@@ -17,7 +17,6 @@ posavska_frontpage_url = 'https://www.nepremicnine.net/oglasi-prodaja/posavska/s
 zasavska_frontpage_url = 'https://www.nepremicnine.net/oglasi-prodaja/zasavska/stanovanje/'
 pomurska_frontpage_url = 'https://www.nepremicnine.net/oglasi-prodaja/pomurska/stanovanje/'
 
-
 st_lj_center = 31
 st_lj_okolica = 6
 st_gorenjska = 6
@@ -32,21 +31,23 @@ st_posavska = 1
 st_zasavska = 1
 st_pomurska = 2
 
-seznam_url = [
-[lj_center_frontpage_url, st_lj_center, 'stanovanja_lj_center.html'],
-[lj_okolica_frontpage_url, st_lj_okolica, 'stanovanja_lj_okolica.html'],
-[gorenjska_frontpage_url, st_gorenjska, 'stanovanja_gorenjska.html'],
-[juzna_primorska_frontpage_url, st_juzna_primorska, 'stanovanja_juzna_primorska.html'],
-[severna_primorska_frontpage_url, st_severna_primorska, 'stanovanja_severna_primorska.html'],
-[notranjska_frontpage_url, st_notranjska, 'stanovanja_notranjska.html'],
-[savinjska_frontpage_url, st_savinjska, 'stanovanja_savinjska.html'],
-[podravska_frontpage_url, st_podravska,'stanovanja_podravska.html'],
-[koroska_frontpage_url, st_koroska, 'stanovanja_koroška.html'],
-[dolenjska_frontpage_url, st_dolenjska, 'stanovanja_dolenjska.html'],
-[posavska_frontpage_url, st_posavska, 'stanovanja_posavska.html'],
-[zasavska_frontpage_url, st_zasavska, 'stanovanja_zasavska.html'],
-[pomurska_frontpage_url, st_pomurska, 'stanovanja_pomurksa.html']
+seznam = [
+[lj_center_frontpage_url, st_lj_center, 'stanovanja_lj_center.html', 'stanovanja_lj_center.csv'],
+[lj_okolica_frontpage_url, st_lj_okolica, 'stanovanja_lj_okolica.html', 'stanovanja_lj_okolica.csv'],
+[gorenjska_frontpage_url, st_gorenjska, 'stanovanja_gorenjska.html', 'stanovanja_gorenjska.csv'],
+[juzna_primorska_frontpage_url, st_juzna_primorska, 'stanovanja_juzna_primorska.html', 'stanovanja_juzna_primorska.csv'],
+[severna_primorska_frontpage_url, st_severna_primorska, 'stanovanja_severna_primorska.html', 'stanovanja_severna_primorska.csv'],
+[notranjska_frontpage_url, st_notranjska, 'stanovanja_notranjska.html', 'stanovanja_notranjska.csv'],
+[savinjska_frontpage_url, st_savinjska, 'stanovanja_savinjska.html', 'stanovanja_savinjska.csv'],
+[podravska_frontpage_url, st_podravska,'stanovanja_podravska.html', 'stanovanja_podravska.csv'],
+[koroska_frontpage_url, st_koroska, 'stanovanja_koroška.html', 'stanovanja_koroška.csv'],
+[dolenjska_frontpage_url, st_dolenjska, 'stanovanja_dolenjska.html', 'stanovanja_dolenjska.csv'],
+[posavska_frontpage_url, st_posavska, 'stanovanja_posavska.html', 'stanovanja_posavska.csv'],
+[zasavska_frontpage_url, st_zasavska, 'stanovanja_zasavska.html', 'stanovanja_zasavska.csv'],
+[pomurska_frontpage_url, st_pomurska, 'stanovanja_pomurska.html', 'stanovanja_pomurska.csv']
 ]
+
+imena_polj = ['lokacija', 'sobno', 'nadstropje', 'leto', 'velikost', 'cena']
 
 stanovanja_directory = 'podatki_stanovanja'
 csv_filename = 'stanovanja'
@@ -80,6 +81,59 @@ def poberi_iz_vseh(seznam_url):
             text += f'{seznam_url[i][0]}' + download_url_to_string(url)
             save_string_to_file(text, stanovanja_directory, datoteka)
 
-#poberi_iz_vseh(seznam_url)
+def page_to_ads(page_content):
+    pattern = r'<div class="oglas_container(.*?)<div class="clearer"></div>' 
+    regexp = re.compile(pattern, re.DOTALL)
+    return re.findall(regexp, page_content)
+
+def vzorec_podatkov_iz_oglasa(block):
+    pattern = (r'.*?<span class="title">(?P<lokacija>.*?)</span></a>.*?' 
+    r'<span class="vrsta">Stanovanje</span><span class="tipi">(?P<sobno>.*?)</span></span>.*?' 
+    r'<span class="atribut">Nadstropje: <strong>(?P<nadstropje>.*?)</strong>.*?' 
+    r'class="atribut leto">Leto: <strong>(?P<leto>.*?)</strong>.*?' 
+    r'<span class="velikost" lang="sl">(?P<velikost>.*?)m2</span>.*?'
+    r'<span class="cena">(?P<cena>.*?) &euro;</span>'
+    )
+    regexp = re.compile(pattern, flags=re.DOTALL)
+    najdeno = re.search(regexp, block)
+    if najdeno:
+        return najdeno.groupdict()
+    else:
+        return None
+
+def seznam_slovarjev(regija_bloki):
+    sez = []
+    for blok in regija_bloki:
+        sez.append(vzorec_podatkov_iz_oglasa(blok))
+    return sez
+
+def pripravi_imenik(ime_datoteke):
+    pripravi_imenik(ime_datoteke)
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
+
+def zapisi_csv(slovarji, imena_polj, ime_datoteke):
+    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
+    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
+        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            if slovar:
+                writer.writerow(slovar)
+
+def izbrisi(sez):
+    while None in sez:
+        sez.remove(None)
+    return sez
+
+def main(seznam):
+    for regija in seznam:
+        vsebina_regije = read_file_to_string(stanovanja_directory, regija[2])
+        seznam_sl = (seznam_slovarjev(page_to_ads(vsebina_regije)))
+        zapisi_csv(izbrisi(seznam_sl), imena_polj, regija[3])
+
+main(seznam)
+
 
 
